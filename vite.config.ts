8d22799e345b81d0f681950a36b604cfee652cb0
Tausif Khan <tautumhare@gmail.com>
2025-11-20 23:59:53 +0530

    update: vite config fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+allowedHosts: ["263f5293.ngrok-free.app"]
+},
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
